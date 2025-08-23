5cc6bdfd8042767c4ba2f749ce42c1cbb567ef27
Itay Levi <itaylh33@gmail.com>
2025-08-23 16:43:13 +0300

    Add the entire website
    When i made this mini-project i didn't plan to put it on GitHub, so im
    importing it to github right now.

---
+++ script.js
@@ -0,0 +1,15 @@
+const colorDisplay = document.getElementById("colorDisplay");
+const colorBtn = document.getElementById("colorBtn");
+
+const getRandomHexColor = () => {
+    let symbols = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += symbols[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+colorBtn.addEventListener("click", () => {
+    colorDisplay.textContent = document.body.style.background = getRandomHexColor();
+});
