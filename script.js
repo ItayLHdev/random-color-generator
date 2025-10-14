0fad554a474d330239239cb75015f03867cc522e
Itay Levi <itaylh33@gmail.com>
2025-10-14 16:54:20 +0300

    Add UI Changes
    new and vibrant buttons animations

--- script.js
+++ script.js
@@ -18,3 +18,33 @@ colorBtn.addEventListener("click", () => {
 copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(colorDisplay.textContent);
 });
+
+const updateButtonClick = () => {
+    function activateButton (button, otherButton) {
+        button.style.borderRadius = otherButton.style.borderRadius = '100px';
+        button.style.transform = 'scale(0.9)';
+    }
+
+    function resetButton (button, otherButton) {
+        button.style.transform = otherButton.style.transform = 'scale(1)';
+        switch (button) {
+            case colorBtn:
+                button.style.borderRadius = '100px 50px 50px 100px';
+                otherButton.style.borderRadius = '50px 100px 100px 50px'
+                break;
+            case copyBtn:
+                button.style.borderRadius = '50px 100px 100px 50px'
+                otherButton.style.borderRadius = '100px 50px 50px 100px';
+                break;
+        }
+    }
+
+    colorBtn.addEventListener("mousedown", () => activateButton(colorBtn, copyBtn));
+    colorBtn.addEventListener("mouseup", () => resetButton(colorBtn, copyBtn));
+    colorBtn.addEventListener("mouseleave", () => resetButton(colorBtn, copyBtn))
+
+    copyBtn.addEventListener("mousedown", () => activateButton(copyBtn, colorBtn));
+    copyBtn.addEventListener("mouseup", () => resetButton(copyBtn, colorBtn));
+    copyBtn.addEventListener("mouseleave", () => resetButton(copyBtn, colorBtn))
+};
+updateButtonClick();
