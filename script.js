7aba9c87e3fb3baffe0a75fb1e8502d571fe78b2
Itay Levi <itaylh33@gmail.com>
2026-06-16 21:49:01 +0300

    Remove useless function

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const colorBtn = document.getElementById("colorBtn");
 const copyBtn = document.getElementById("copy");
 
 const getRandomHexColor = () => {
-    let symbols = "0123456789ABCDEF";
+    const HEX_SYMBOLS = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-        color += symbols[Math.floor(Math.random() * 16)];
+        color += HEX_SYMBOLS[Math.floor(Math.random() * 16)];
     }
     return color;
 };
@@ -23,32 +23,29 @@ copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(colorDisplay.textContent);
 });
 
-const updateButtonClick = () => {
-    function activateButton (button, otherButton) {
-        button.style.borderRadius = otherButton.style.borderRadius = '100px';
-        button.style.transform = 'scale(0.9)';
+function activateButton (button, otherButton) {
+    button.style.borderRadius = otherButton.style.borderRadius = '100px';
+    button.style.transform = 'scale(0.9)';
+}
+
+function resetButton (button, otherButton) {
+    button.style.transform = otherButton.style.transform = 'scale(1)';
+    switch (button) {
+        case colorBtn:
+            button.style.borderRadius = '100px 50px 50px 100px';
+            otherButton.style.borderRadius = '50px 100px 100px 50px'
+            break;
+        case copyBtn:
+            button.style.borderRadius = '50px 100px 100px 50px'
+            otherButton.style.borderRadius = '100px 50px 50px 100px';
+            break;
     }
+}
 
-    function resetButton (button, otherButton) {
-        button.style.transform = otherButton.style.transform = 'scale(1)';
-        switch (button) {
-            case colorBtn:
-                button.style.borderRadius = '100px 50px 50px 100px';
-                otherButton.style.borderRadius = '50px 100px 100px 50px'
-                break;
-            case copyBtn:
-                button.style.borderRadius = '50px 100px 100px 50px'
-                otherButton.style.borderRadius = '100px 50px 50px 100px';
-                break;
-        }
-    }
+colorBtn.addEventListener("mousedown", () => activateButton(colorBtn, copyBtn));
+colorBtn.addEventListener("mouseup", () => resetButton(colorBtn, copyBtn));
+colorBtn.addEventListener("mouseleave", () => resetButton(colorBtn, copyBtn))
 
-    colorBtn.addEventListener("mousedown", () => activateButton(colorBtn, copyBtn));
-    colorBtn.addEventListener("mouseup", () => resetButton(colorBtn, copyBtn));
-    colorBtn.addEventListener("mouseleave", () => resetButton(colorBtn, copyBtn))
-
-    copyBtn.addEventListener("mousedown", () => activateButton(copyBtn, colorBtn));
-    copyBtn.addEventListener("mouseup", () => resetButton(copyBtn, colorBtn));
-    copyBtn.addEventListener("mouseleave", () => resetButton(copyBtn, colorBtn))
-};
-updateButtonClick();
+copyBtn.addEventListener("mousedown", () => activateButton(copyBtn, colorBtn));
+copyBtn.addEventListener("mouseup", () => resetButton(copyBtn, colorBtn));
+copyBtn.addEventListener("mouseleave", () => resetButton(copyBtn, colorBtn))
