8615adab3ebde487db86406574dd284471306a3b
Itay Levi <itaylh33@gmail.com>
2026-06-17 12:58:27 +0300

    Clean the code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const colorDisplay = document.getElementById("colorDisplay");
-const colorBtn = document.getElementById("colorBtn");
-const copyBtn = document.getElementById("copy");
+const colorDisplay = document.querySelector("#colorDisplay");
+const colorBtn = document.querySelector("#colorBtn");
+const copyBtn = document.querySelector("#copy");
 
+const HEX_SYMBOLS = "0123456789ABCDEF";
 const getRandomHexColor = () => {
-    const HEX_SYMBOLS = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-        color += HEX_SYMBOLS[Math.floor(Math.random() * 16)];
+        color += HEX_SYMBOLS[Math.floor(Math.random() * HEX_SYMBOLS.length)];
     }
     return color;
 };
@@ -14,7 +14,7 @@ const getRandomHexColor = () => {
 colorBtn.addEventListener("click", () => {
     const color = getRandomHexColor();
     colorDisplay.textContent = color;
-    document.body.style.background = color;
+    document.body.style.backgroundColor = color;
     colorBtn.style.color = color;
     copyBtn.style.color = color;
 });
@@ -24,28 +24,20 @@ copyBtn.addEventListener("click", () => {
 });
 
 function activateButton (button, otherButton) {
-    button.style.borderRadius = otherButton.style.borderRadius = '100px';
-    button.style.transform = 'scale(0.9)';
+    button.classList.add("button-active");
+    otherButton.classList.add("button-other-active");
 }
 
 function resetButton (button, otherButton) {
-    button.style.transform = otherButton.style.transform = 'scale(1)';
-    switch (button) {
-        case colorBtn:
-            button.style.borderRadius = '100px 50px 50px 100px';
-            otherButton.style.borderRadius = '50px 100px 100px 50px'
-            break;
-        case copyBtn:
-            button.style.borderRadius = '50px 100px 100px 50px'
-            otherButton.style.borderRadius = '100px 50px 50px 100px';
-            break;
-    }
+    button.classList.remove("button-active");
+    otherButton.classList.remove("button-other-active");
 }
 
-colorBtn.addEventListener("mousedown", () => activateButton(colorBtn, copyBtn));
-colorBtn.addEventListener("mouseup", () => resetButton(colorBtn, copyBtn));
-colorBtn.addEventListener("mouseleave", () => resetButton(colorBtn, copyBtn))
+function addButtonListeners (button, otherButton) {
+    button.addEventListener("mousedown", () => activateButton(button, otherButton));
+    button.addEventListener("mouseup", () => resetButton(button, otherButton));
+    button.addEventListener("mouseleave", () => resetButton(button, otherButton))
+}
 
-copyBtn.addEventListener("mousedown", () => activateButton(copyBtn, colorBtn));
-copyBtn.addEventListener("mouseup", () => resetButton(copyBtn, colorBtn));
-copyBtn.addEventListener("mouseleave", () => resetButton(copyBtn, colorBtn))
+addButtonListeners(colorBtn, copyBtn)
+addButtonListeners(copyBtn, colorBtn)
